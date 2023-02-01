55dfdd917171180e75f630cfb7b3b2108d0ec09c
Jens gerwins <Jens.gerwins@gmail.com>
2023-02-01 20:57:43 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("hello world");
+console.log("J"+ "e" + "n"+ "s");
+console.log("Hallo");
+console.log(`"That doesnt" "work"`);
+console.log(50 + 2 == 52);
+console.log("333" != 333);
+console.log(600 / 510 / 2);
+console.log("2" + 2 - 20);
+console.log(25 /5 === 50/10);
